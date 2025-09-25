a9500753fce5fc7178561e1f7c618ea1b2c7b24d
dsham7 <81854361+dsham7@users.noreply.github.com>
2025-09-25 15:35:34 +0530

    deployment essential lines

--- metric_viewer_v3/app.py
+++ metric_viewer_v3/app.py
@@ -4,6 +4,7 @@ import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True, use_pages=True)
 app.title = "Business Metric Viewer"
+server = app.server
 from components.navbar import navbar
 
 app.layout = html.Div([
@@ -14,4 +15,5 @@ app.layout = html.Div([
 ])
 
 if __name__ == '__main__':
-    app.run(host='localhost',port='5000',debug=True)
+    # app.run(host='localhost',port='5000',debug=True) # for local testing
+    app.run_server(host='0.0.0.0', port=8050, debug=False) # for deployment
